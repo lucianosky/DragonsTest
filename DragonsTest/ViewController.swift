431f22971b36da8ddbfc5de57574e549a1810f44
Luciano Sclovsky <luciano.sclovsky@softdesign-rs.com.br>
2019-05-27 13:34:49 -0300

    Created Xcode project

---
+++ DragonsTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DragonsTest
+//
+//  Created by Luciano Sclovsky on 27/05/19.
+//  Copyright © 2019 Luciano Sclovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DragonsTestTests/DragonsTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  DragonsTestTests.swift
+//  DragonsTestTests
+//
+//  Created by Luciano Sclovsky on 27/05/19.
+//  Copyright © 2019 Luciano Sclovsky. All rights reserved.
+//
+
+import XCTest
+@testable import DragonsTest
+
+class DragonsTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
